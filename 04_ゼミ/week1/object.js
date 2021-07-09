00c03caa9f7fdc76a41f141581904b22a4e18935
kokoyume <yumeyumeturbo@outlook.jp>
2021-07-10 08:11:09 +0900

    テスト

---
+++ 04_ゼミ/week1/object.js
@@ -0,0 +1,16 @@
+const yumeka = {
+    name:"小島　夢輝"
+    sayHello: function() {
+        alert("こんにちは!どりです！")
+      },
+    universitiy:"和歌山大学"
+    age:20
+    hobby:["プリン","チョコ","ポテチ"]
+    familly:(papa:"なるひと"　mama:"あかね") 
+
+}
+
+console.log(yumeka.name)
+console.log(yumeka.age)
+console.log(yumeka.hobby[0])
+console.log(yumeka.familly.papa)
